f556c7559970c31b5536d68834e0bbf5de849968
Thomas Parrott <tomp@tomp.uk>
2020-08-07 07:40:44 +0100

    ovn provider example
    Signed-off-by: Thomas Parrott <tomp@tomp.uk>

--- ovn_external_provider_ovs.sh
+++ ovn_external_provider_ovs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Setup external provider using OVS bridge.
+
+# LXD network on per-node network startup.
 ip link add dev ovs-lxdbr0-a type veth peer name ovs-lxdbr0-b
 sysctl net.ipv6.conf.ovs-lxdbr0-a.disable_ipv6=1 \
         net.ipv6.conf.ovs-lxdbr0-a.forwarding=0 \
@@ -7,6 +9,8 @@ sysctl net.ipv6.conf.ovs-lxdbr0-a.disable_ipv6=1 \
         net.ipv6.conf.ovs-lxdbr0-b.forwarding=0
 ip link set master lxdbr0 up ovs-lxdbr0-a
 ip link set ovs-lxdbr0-b up
+
+# SysAdmin
 ovs-vsctl --may-exist add-br ovs-lxdbr0
 ovs-vsctl --may-exist add-port ovs-lxdbr0 ovs-lxdbr0-b
 ovs-vsctl set open . external-ids:ovn-bridge-mappings=lxdbr0:ovs-lxdbr0
