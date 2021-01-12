9a5a76edcdfd721ecca9e4de10a0a3b696655f70
Thomas Parrott <tomp@tomp.uk>
2021-01-12 17:04:16 +0000

    updates ovn acl to add default drop rule per NSG, and create-time baseline allow rules for logical switches
    Signed-off-by: Thomas Parrott <tomp@tomp.uk>

--- ovn-acl.sh
+++ ovn-acl.sh
@@ -10,25 +10,7 @@ dnsIP6="fd42:442d:225e:59df::1"
 # Clear all rules for a switch.
 sudo ovn-nbctl clear logical_switch "${switchName}" acls
 
-# Delete port groups.
-sudo ovn-nbctl --if-exists destroy port_group myrouters
-sudo ovn-nbctl --if-exists destroy port_group mynicsping
-sudo ovn-nbctl --if-exists destroy port_group mynicspinggoogle
-
-# Setup NIC ping port group.
-sudo ovn-nbctl pg-add mynicsping
-sudo ovn-nbctl pg-set-ports mynicsping lxd-net9-instance-37b963ca-378c-4eca-bbf0-e4cdfcbd62e0-eth0 lxd-net9-instance-9211022a-4df4-4122-9588-525401779289-eth0
-
-# Setup NIC ping 8.8.8.8 port group.
-sudo ovn-nbctl pg-add mynicspinggoogle
-sudo ovn-nbctl pg-set-ports mynicspinggoogle lxd-net9-instance-37b963ca-378c-4eca-bbf0-e4cdfcbd62e0-eth0 lxd-net9-instance-9211022a-4df4-4122-9588-525401779289-eth0
-
-# Setup router port group.
-sudo ovn-nbctl pg-add myrouters
-sudo ovn-nbctl pg-set-ports myrouters "${routerPort}"
-
-# Baseline switch rules.
-sudo ovn-nbctl --type=switch --log acl-add "${switchName}" to-lport 0 1 drop # Default log and drop
+# Baseline allow switch rules added when switch is created.
 sudo ovn-nbctl --type=switch acl-add "${switchName}" to-lport 1 "arp" allow # ARP
 sudo ovn-nbctl --type=switch acl-add "${switchName}" to-lport 1 "nd" allow # Neighbour discovery
 sudo ovn-nbctl --type=switch acl-add "${switchName}" to-lport 1 "icmp6.type == 143" allow # Multicast listener report
@@ -45,10 +27,43 @@ sudo ovn-nbctl --type=switch acl-add "${switchName}" to-lport 1 "outport == \"${
 sudo ovn-nbctl --type=switch acl-add "${switchName}" to-lport 1 "outport == \"${routerPort}\" && ip6.dst == ${dnsIP6} && udp.dst == 53" allow # DNS IPv6 UDP
 sudo ovn-nbctl --type=switch acl-add "${switchName}" to-lport 1 "outport == \"${routerPort}\" && ip6.dst == ${dnsIP6} && tcp.dst == 53" allow # DNS IPv6 TCP
 
-# NIC port group rules.
-sudo ovn-nbctl --type=port-group acl-add mynicsping to-lport 2 "inport == @mynicsping && outport == @mynicsping && icmp" allow-related # Ping between ports in group
-sudo ovn-nbctl --type=port-group acl-add mynicspinggoogle to-lport 2 "inport == @mynicspinggoogle && icmp && ip4.dst == 8.8.8.8" allow-related # Ping google from ports in group
+# Setup NSG port group to allow ping internally.
+sudo ovn-nbctl --if-exists destroy port_group ping_internal
+sudo ovn-nbctl pg-add ping_internal
+sudo ovn-nbctl --type=port-group acl-add ping_internal to-lport 2 "inport == @ping_internal && outport == @ping_internal && icmp" allow-related
+sudo ovn-nbctl --type=port-group --log --name ping_internal acl-add ping_internal to-lport 0 "inport == @ping_internal || outport == @ping_internal" drop # NSG default drop.
 
-# Router port group rules.
-sudo ovn-nbctl --type=port-group acl-add myrouters to-lport 2 "outport == @myrouters && tcp.dst == {80,443}" allow # HTTP{S} outbound
-sudo ovn-nbctl --type=port-group acl-add myrouters to-lport 2 "inport == @myrouters && icmp && ip4.dst == 10.0.0.1" allow # Ping to external route NIC IP
+# Apply NSG to multiple NICs.
+sudo ovn-nbctl pg-set-ports ping_internal \
+        lxd-net9-instance-37b963ca-378c-4eca-bbf0-e4cdfcbd62e0-eth0 \
+        lxd-net9-instance-9211022a-4df4-4122-9588-525401779289-eth0
+
+# Setup NSG port group to allow ping to 8.8.8.8.
+sudo ovn-nbctl --if-exists destroy port_group ping_google
+sudo ovn-nbctl pg-add ping_google
+sudo ovn-nbctl --type=port-group acl-add ping_google to-lport 2 "inport == @ping_google && icmp && ip4.dst == 8.8.8.8" allow-related
+sudo ovn-nbctl --type=port-group --log --name ping_google acl-add ping_google to-lport 0 "inport == @ping_google || outport == @ping_google" drop # NSG default drop.
+
+# Apply NSG to single NIC.
+sudo ovn-nbctl pg-set-ports ping_google \
+        lxd-net9-instance-37b963ca-378c-4eca-bbf0-e4cdfcbd62e0-eth0
+
+# Setup NSG port group to allow HTTP externally.
+sudo ovn-nbctl --if-exists destroy port_group http_outbound
+sudo ovn-nbctl pg-add http_outbound
+sudo ovn-nbctl --type=port-group acl-add http_outbound to-lport 2 "outport == @http_outbound && tcp.dst == {80,443}" allow # HTTP{S} outbound
+sudo ovn-nbctl --type=port-group --log --name http_outbound acl-add http_outbound to-lport 0 "inport == @http_outbound || outport == @http_outbound" drop # NSG default drop.
+
+# Apply NSG to network by adding router port to group.
+sudo ovn-nbctl pg-set-ports http_outbound \
+        "${routerPort}"
+
+# Setup NSG port group to allow ping to our externally routed subnet.
+sudo ovn-nbctl --if-exists destroy port_group ping_external_subnet
+sudo ovn-nbctl pg-add ping_external_subnet
+sudo ovn-nbctl --type=port-group acl-add ping_external_subnet to-lport 2 "icmp && ip4.dst == 10.0.0.0/24" allow-related
+sudo ovn-nbctl --type=port-group --log --name ping_external_subnet acl-add ping_external_subnet to-lport 0 "inport == @ping_internal || outport == @ping_internal" drop # NSG default drop.
+
+# Apply NSG to network by adding router port to group.
+sudo ovn-nbctl pg-set-ports ping_external_subnet \
+        "${routerPort}"
